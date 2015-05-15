1e05eecc40a710752045ee0cb26c282bf9266abd
sprabakkar@gmail.com <psingaram@gmail.com>
2015-05-15 11:20:26 -0400

    premium object updated

--- brms-spring-mvc-workshop/domain-model/src/main/java/com/redhat/brms/Premium.java
+++ brms-spring-mvc-workshop/domain-model/src/main/java/com/redhat/brms/Premium.java
@@ -1,5 +1,22 @@
 package com.redhat.brms;
 
+import java.math.BigDecimal;
+
 public class Premium {
+private String type;
+private BigDecimal amount;
+public String getType() {
+	return type;
+}
+public void setType(String type) {
+	this.type = type;
+}
+public BigDecimal getAmount() {
+	return amount;
+}
+public void setAmount(BigDecimal amount) {
+	this.amount = amount;
+}
+
 
 }
