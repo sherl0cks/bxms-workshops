f49fe849ac50422139069a13dae17aa71c67db65
sprabakkar@gmail.com <psingaram@gmail.com>
2015-05-15 11:22:17 -0400

    quote object updated with attributes

--- brms-spring-mvc-workshop/domain-model/src/main/java/com/redhat/brms/Quote.java
+++ brms-spring-mvc-workshop/domain-model/src/main/java/com/redhat/brms/Quote.java
@@ -1,5 +1,21 @@
 package com.redhat.brms;
 
+import java.math.BigDecimal;
+
 public class Quote {
+private String eligibility;
+private BigDecimal totalPremium;
+public String getEligibility() {
+	return eligibility;
+}
+public void setEligibility(String eligibility) {
+	this.eligibility = eligibility;
+}
+public BigDecimal getTotalPremium() {
+	return totalPremium;
+}
+public void setTotalPremium(BigDecimal totalPremium) {
+	this.totalPremium = totalPremium;
+}
 
 }
