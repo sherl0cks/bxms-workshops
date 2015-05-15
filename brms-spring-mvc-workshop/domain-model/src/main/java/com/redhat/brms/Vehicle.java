299879558158a50ecbe6d38b66a73daa24c3d602
sprabakkar@gmail.com <psingaram@gmail.com>
2015-05-15 11:28:06 -0400

    updated

--- brms-spring-mvc-workshop/domain-model/src/main/java/com/redhat/brms/Vehicle.java
+++ brms-spring-mvc-workshop/domain-model/src/main/java/com/redhat/brms/Vehicle.java
@@ -1,34 +1,41 @@
 package com.redhat.brms;
 
 public class Vehicle {
-private String vin;
-private String model;
-private int year;
-private String make;
+	private String vin;
+	private String model;
+	private int year;
+	private String make;
 
-public String getVin() {
-	return vin;
-}
-public void setVin(String vin) {
-	this.vin = vin;
-}
-public String getModel() {
-	return model;
-}
-public void setModel(String model) {
-	this.model = model;
-}
-public int getYear() {
-	return year;
-}
-public void setYear(int year) {
-	this.year = year;
-}
-public String getMake() {
-	return make;
-}
-public void setMake(String make) {
-	this.make = make;
-}
+	public String getVin() {
+		return vin;
+	}
+
+	public void setVin(String vin) {
+		this.vin = vin;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
 
 }
