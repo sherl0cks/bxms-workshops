a9bf9032426527513d55224dfd03735babedb37c
Justin Holmes <justinmichaelholmes@gmail.com>
2015-05-16 19:04:49 -0400

    created basic web app

---
+++ brms-spring-mvc-workshop/spring-mvc-war/src/main/java/com/redhat/mvc/WebController.java
@@ -0,0 +1,16 @@
+package com.redhat.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/index")
+public class WebController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String printHello(ModelMap model) {
+		return "index";
+	}
+}
---
+++ brms-spring-mvc-workshop/spring-mvc-war/src/main/webapp/WEB-INF/pages/template/menu.jsp
@@ -0,0 +1,8 @@
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<ul style="list-style:none;line-height:28px;">
+
+	<li>
+    <spring:url value="/index" var="homeUrl" htmlEscape="true" />
+		<a href="${homeUrl}">Home</a>
+	</li>
+</ul>
