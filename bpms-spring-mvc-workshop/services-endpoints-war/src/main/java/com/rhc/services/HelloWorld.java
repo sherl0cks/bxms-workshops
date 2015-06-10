7280ad2fea710d1a7311d183039be386672c5244
Justin Holmes <justinmichaelholmes@gmail.com>
2015-06-09 21:00:57 -0400

    camel integrated

--- bpms-spring-mvc-workshop/services-endpoints-war/src/main/java/com/rhc/services/HelloWorld.java
+++ bpms-spring-mvc-workshop/services-endpoints-war/src/main/java/com/rhc/services/HelloWorld.java
@@ -3,6 +3,8 @@ package com.rhc.services;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,12 +15,17 @@ public class HelloWorld {
 	@Autowired
 	private Foo foo;
 
+	@Produce(uri = "direct:out")
+	private ProducerTemplate template;
+
 	@GET
 	public String getRooms() {
-		if (foo == null) {
-			System.err.println("it is null");
+		if (template == null) {
+			System.err.println("error");
 		} else {
-			System.err.println(foo.getName());
+			String response = template.requestBody("direct:in", "fooooo", String.class);
+			System.err.println(response);
+
 		}
 		System.err.println("test3");
 		return "hello world";
@@ -31,6 +38,14 @@ public class HelloWorld {
 
 	public void setFoo(Foo foo) {
 		this.foo = foo;
+	}
+
+	public ProducerTemplate getTemplate() {
+		return template;
+	}
+
+	public void setTemplate(ProducerTemplate template) {
+		this.template = template;
 	}
 
 }
