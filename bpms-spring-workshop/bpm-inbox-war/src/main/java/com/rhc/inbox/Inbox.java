2ae0be1b9ace8e4c48cf8438f6a45d9d67849223
Justin Holmes <justinmichaelholmes@gmail.com>
2015-06-15 18:48:40 -0400

    building endpoint

---
+++ bpms-spring-workshop/bpm-inbox-war/src/main/java/com/rhc/inbox/Inbox.java
@@ -0,0 +1,35 @@
+package com.rhc.inbox;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Inbox {
+
+	private Long id;
+
+	private Collection<Task> tasks;
+
+	public Collection<Task> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(Collection<Task> tasks) {
+		this.tasks = tasks;
+	}
+
+	public void addTask(Task task) {
+		if (tasks == null) {
+			this.tasks = new ArrayList<>();
+		}
+		this.tasks.add(task);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
