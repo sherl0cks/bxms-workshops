84b4563eb97acffc16046f67ec85e9da76e9c9fd
Justin Holmes <justinmichaelholmes@gmail.com>
2015-07-23 02:45:47 -0400

    adding test info for timer utils process

--- bpm-spring-workshop/bpm-services/src/test/java/com/rhc/bpm/AsyncProcessTest.java
+++ bpm-spring-workshop/bpm-services/src/test/java/com/rhc/bpm/AsyncProcessTest.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.jbpm.examples.cmd.ForcedRetryCommand;
+import org.jbpm.examples.cmd.PollForcedRetryCommand;
 import org.jbpm.kie.services.impl.KModuleDeploymentUnit;
 import org.jbpm.services.api.model.DeploymentUnit;
 import org.junit.Assert;
@@ -22,6 +23,8 @@ public class AsyncProcessTest extends AbstractBpmServiceTest {
 	public static void init() {
 		if (System.getProperty("org.kie.deployment.desc.location") == null) {
 			System.setProperty("org.kie.deployment.desc.location", "classpath:META-INF/kie-wb-deployment-descriptor.xml");
+		}if (System.getProperty("org.kie.executor.interval") == null) {
+			System.setProperty("org.kie.executor.interval", "1");
 		}
 	}
 
@@ -30,21 +33,16 @@ public class AsyncProcessTest extends AbstractBpmServiceTest {
 		// given
 		deploymentService.deploy(DEPLOYMENT_UNIT);
 		Map<String, Object> params = new HashMap<String, Object>();
-		params.put("command", "org.jbpm.examples.cmd.ForcedRetryCommand");
-		params.put("completionCheck", new Integer(0));
-		params.put("isComplete", new Boolean(false));
-		params.put("maxCompletionCheck", new Integer(2));
+		params.put("sendCommand", "org.jbpm.examples.cmd.ForcedRetryCommand");
+		params.put("pollCommand", "org.jbpm.examples.cmd.PollForcedRetryCommand");
 
 		// when
 		Long id = processService.startProcess(DEPLOYMENT_UNIT.getIdentifier(), PROCESS_ID, params);
 
-		Thread.sleep(10000l);
+		Thread.sleep(20000l);
 
 		// then
 		Assert.assertEquals(2, ForcedRetryCommand.executionCount);
-
-		int completionCheck = 0;
-
-
+		Assert.assertEquals(3, PollForcedRetryCommand.pollCount);
 	}
 }
